d6c0aa9fbadefe0edda1a2abd2a57fcc3647d996
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-01 09:53:30 +0700

    Update messages.py

--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -5,6 +5,12 @@ from bot.api import MobileTikTokAPI, TikTokAPI
 
 platforms = [MobileTikTokAPI(), TikTokAPI()]
 
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hai Aku Adalah Bot Untuk Mendownload Video Tiktok Tanpa Watermark. Silahkan Kirim Videonya Kesini. Powered By @aiogram!")
 
 @dp.message_handler()
 async def get_message(message: Message):
