f2ffc08072768582cbc068444d8d9699052cf96e
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-01 11:55:24 +0700

    Delete keyboard.py

--- bot/handlers/keyboard.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-
-#some code
-
-studyboi = InlineKeyboardButton('Channel Bot', url='https://t.me/nekozu')
-start_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(studyboi)
