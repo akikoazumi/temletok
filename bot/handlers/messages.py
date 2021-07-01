c7c743f9ca22e998a91132449f42ac1749a51766
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-01 09:59:51 +0700

    Update messages.py

--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -1,7 +1,7 @@
 from aiogram.types import Message
 from bot import dp, bot
 from bot.api import MobileTikTokAPI, TikTokAPI
-
+import keyboards as kb
 
 platforms = [MobileTikTokAPI(), TikTokAPI()]
 
@@ -10,7 +10,7 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hai Aku Adalah Bot Untuk Mendownload Video Tiktok Tanpa Watermark. Silahkan Kirim Videonya Kesini. Powered By @aiogram!")
+    await message.reply("Hai Aku Adalah Bot Untuk Mendownload Video Tiktok Tanpa Watermark. Silahkan Kirim Videonya Kesini. Powered By @aiogram!", reply_markup=kb.start_keyboard)
 
 @dp.message_handler()
 async def get_message(message: Message):
