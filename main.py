c82c838d0d19577cc6b6fc5fcff7ef657cd5b4d5
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-01 09:50:15 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+
+import sentry_sdk
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
+
+from bot import bot, dp, handlers  # noqa
+from settings import ENVIRONMENT, SENTRY_DSN
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    environment=ENVIRONMENT,
+    integrations=[AioHttpIntegration()]
+)
+
+
+async def main():
+    try:
+        logging.info('Started')
+        await dp.start_polling()
+    finally:
+        logging.info('Exited')
+        await bot.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
